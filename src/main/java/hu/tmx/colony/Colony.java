7eb72798a8054222563ec2fca93f4ff13b57d11b
figlerr <figler.renata@tmx.hu>
2021-07-06 16:27:52 +0200

    forras

--- src/main/java/hu/tmx/colony/Colony.java
+++ src/main/java/hu/tmx/colony/Colony.java
@@ -1,6 +1,9 @@
 package hu.tmx.colony;
 
+import java.util.ArrayList;
+
 import static hu.tmx.colony.RandomGenerator.antsRandom;
+import static hu.tmx.colony.RandomGenerator.stepsRandom;
 
 public class Colony {
 
@@ -14,10 +17,20 @@ public class Colony {
     }
 
     private void generateAnts(int workers, int soldiers, int drones ){
-
+        ArrayList<Worker> workerAnts = new ArrayList<Worker>();
+        for (int i = 0; i < workers; i++){
+            workerAnts.add(new Worker(stepsRandom(), stepsRandom()));
+        }
+        ArrayList<Soldier> soldierAnts = new ArrayList<Soldier>();
+        ArrayList<Drone> droneAnts = new ArrayList<Drone>();
 
     }
 
+    private Ants getAnts(String antsVariety, int number){
+        for (int i = 0; i < number; i++){
+            workerAnts.add(new + 'antsVariety'(stepsRandom(), stepsRandom()));
+        }
+    }
 
 
 }
--- src/main/java/hu/tmx/colony/RandomGenerator.java
+++ src/main/java/hu/tmx/colony/RandomGenerator.java
@@ -5,4 +5,8 @@ public class RandomGenerator {
     public static int antsRandom(){
         return (int) (Math.random() * 5);
     }
+
+    public static int stepsRandom(){
+        return (int) (Math.random() * 101);
+    }
 }
